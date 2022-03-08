8d6b4e68acb0edd2186bfbd4810c66d4b6b9f47d
Best <best@biggestfan.net>
2022-03-08 20:32:50 +0700

    add inspector

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { inspect } from "@xstate/inspect";
 import { interpret } from "xstate";
 import { descriptionMachine, services } from "./description-machine";
 // @ts-ignore
@@ -9,6 +10,12 @@ const elm = Elm.Main.init({
 });
 
 const machine = descriptionMachine(services);
+
+inspect({
+  // options
+  // url: 'https://statecharts.io/inspect', // (default)
+  iframe: false, // open in new window
+});
 
 const machineInterpreter = interpret(machine, {
   devTools: true,
