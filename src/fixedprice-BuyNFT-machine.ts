9178092061ea5b04b3359e723242e28ac952fefc
Best <best@biggestfan.net>
2022-03-15 21:50:14 +0700

    update buyNFTMachine

--- src/fixedprice-BuyNFT-machine.ts
+++ src/fixedprice-BuyNFT-machine.ts
@@ -1,29 +1,107 @@
 import { assign, createMachine } from "xstate";
 
+export const services = {
+  checkConnectWallet: async () => {},
+  fetchWhitelistStatus: async (wallet_address) => {
+    let results: Whitelist;
+    fetch(
+      "/services/fixed-price/ampleia/presale-1/whitelist/0x9b4c4286cd67f9E20e79B1f8Efd25350646f5b0B",
+      {
+        method: "GET",
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          accept: "application/json",
+          Authorization: "Bearer xxxxx",
+        },
+      }
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        console.log("whitelist res :", res);
+        results = res;
+      });
+    return results;
+  },
+  buyNFTRequest: async () => {
+    throw new Error();
+  },
+};
+
+interface Whitelist {
+  project_id: string;
+  round_id: string;
+  wallet_address: string;
+  total_quota: number;
+  remaining_quota: number;
+  meta: {
+    tier: string;
+    point: string;
+    level: string;
+  };
+}
+
 export const buyNFTMachine = createMachine(
   {
     id: "BUYNFT",
     tsTypes: {} as import("./fixedprice-BuyNFT-machine.typegen").Typegen0,
     schema: {
-      context: {} as { quantity: number },
+      context: {} as { whitelistData: Whitelist; quantity: number },
       events: {} as
         | { type: "BUYNFT.CLICKED" }
         | { type: "QUANTITY.INCREASECLICKED" }
-        | { type: "QUANTITY.DECREASECLICKED" },
-      services: {} as {},
+        | { type: "QUANTITY.DECREASECLICKED" }
+        | { type: "QUOTA.CHECKED" },
+      services: {} as {
+        fetchWhitelistStatus: { data: Whitelist };
+      },
     },
     context: {
+      whitelistData: null,
       quantity: 1,
     },
     states: {
-      idle: {
+      check_walletConnection: {
+        invoke: {
+          id: "connectwallet",
+          src: "checkConnectWallet",
+          onDone: {
+            target: "check_whitelist",
+          },
+          onError: {
+            target: "cannot_buy",
+          },
+        },
+      },
+      check_whitelist: {
+        invoke: {
+          id: "fetch-whiteliststatus",
+          src: "fetchWhitelistStatus",
+          onDone: {
+            target: "check_quota",
+            actions: "updateWhitelistStatus",
+          },
+          onError: {
+            target: "cannot_buy",
+          },
+        },
+      },
+      check_quota: {
+        on: {
+          "QUOTA.CHECKED": [
+            { target: "cannot_buy", cond: "quotaOff" },
+            { target: "ready_to_buy", cond: "quotaRemains" },
+          ],
+        },
+      },
+      cannot_buy: {},
+      ready_to_buy: {
         on: {
           "QUANTITY.INCREASECLICKED": {
-            target: "idle",
+            target: "ready_to_buy",
             actions: "increaseQuantity",
           },
           "QUANTITY.DECREASECLICKED": {
-            target: "idle",
+            target: "ready_to_buy",
             actions: "decreaseQuantity",
           },
           "BUYNFT.CLICKED": {
@@ -46,10 +124,17 @@ export const buyNFTMachine = createMachine(
       complete: {},
       error: {},
     },
-    initial: "idle",
+    initial: "check_walletConnection",
   },
   {
+    services,
     actions: {
+      updateWhitelistStatus: assign((context, event) => {
+        return {
+          ...context,
+          whitelistData: event.data,
+        };
+      }),
       increaseQuantity: assign((context) => {
         const q = context.quantity === 10 ? 10 : context.quantity + 1;
         console.log("increase action", q);
@@ -67,10 +152,9 @@ export const buyNFTMachine = createMachine(
         };
       }),
     },
-    services: {
-      buyNFTRequest: async () => {
-        throw new Error();
-      },
+    guards: {
+      quotaOff: (context) => context.whitelistData.remaining_quota < 1,
+      quotaRemains: (context) => context.whitelistData.remaining_quota > 1,
     },
   }
 );
--- src/fixedprice-BuyNFT-machine.typegen.ts
+++ src/fixedprice-BuyNFT-machine.typegen.ts
@@ -3,11 +3,30 @@
 export interface Typegen0 {
   "@@xstate/typegen": true;
   eventsCausingActions: {
+    updateWhitelistStatus: "done.invoke.fetch-whiteliststatus";
     increaseQuantity: "QUANTITY.INCREASECLICKED";
     decreaseQuantity: "QUANTITY.DECREASECLICKED";
   };
   internalEvents: {
+    "done.invoke.fetch-whiteliststatus": {
+      type: "done.invoke.fetch-whiteliststatus";
+      data: unknown;
+      __tip: "See the XState TS docs to learn how to strongly type this.";
+    };
+    "done.invoke.connectwallet": {
+      type: "done.invoke.connectwallet";
+      data: unknown;
+      __tip: "See the XState TS docs to learn how to strongly type this.";
+    };
     "xstate.init": { type: "xstate.init" };
+    "error.platform.connectwallet": {
+      type: "error.platform.connectwallet";
+      data: unknown;
+    };
+    "error.platform.fetch-whiteliststatus": {
+      type: "error.platform.fetch-whiteliststatus";
+      data: unknown;
+    };
     "done.invoke.buyNFT-request": {
       type: "done.invoke.buyNFT-request";
       data: unknown;
@@ -19,6 +38,8 @@ export interface Typegen0 {
     };
   };
   invokeSrcNameMap: {
+    checkConnectWallet: "done.invoke.connectwallet";
+    fetchWhitelistStatus: "done.invoke.fetch-whiteliststatus";
     buyNFTRequest: "done.invoke.buyNFT-request";
   };
   missingImplementations: {
@@ -28,10 +49,23 @@ export interface Typegen0 {
     delays: never;
   };
   eventsCausingServices: {
+    checkConnectWallet: "xstate.init";
+    fetchWhitelistStatus: "done.invoke.connectwallet";
     buyNFTRequest: "BUYNFT.CLICKED";
   };
-  eventsCausingGuards: {};
+  eventsCausingGuards: {
+    quotaOff: "QUOTA.CHECKED";
+    quotaRemains: "QUOTA.CHECKED";
+  };
   eventsCausingDelays: {};
-  matchesStates: "idle" | "pending" | "complete" | "error";
+  matchesStates:
+    | "check_walletConnection"
+    | "check_whitelist"
+    | "check_quota"
+    | "cannot_buy"
+    | "ready_to_buy"
+    | "pending"
+    | "complete"
+    | "error";
   tags: never;
 }
